f5e8e4f89402a2a40f2a08d7c8f1651169ec139c
Marcelo Bertoldi Diani <marcelodianib@gmail.com>
2018-05-24 11:13:11 -0300

    Bessel functions added

--- findBeta.m
+++ findBeta.m
@@ -12,9 +12,15 @@ function findBeta(lambda, n1, n2, a)
     
     k0 = 2*pi/lambda; 
     u = sqrt(k0^2 * n1^2 - b^2);
+    w = sqrt(b^2 - k0^2 * n2^2);
     nu = 0;
     
+    jnu = besselj(nu,u*a);
+    knu = besselk(nu,w*a);
+    djnu = (besselj(nu-1,u*a) - besselj(nu+1,u*a))/2;
+    dknu = (besselk(nu-1,w*a) + besselk(nu+1,w*a))/2;
     
+        
     matrix = zeros(4,4);
     
     matrix(1,3) = 0;
