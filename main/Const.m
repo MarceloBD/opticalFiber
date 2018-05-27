d77273ae7a5f8f73f8e5d1fb0d17229ddf47fa0c
Marcelo Bertoldi Diani <marcelodianib@gmail.com>
2018-05-27 18:54:41 -0300

    Translating to english

--- main/Const.m
+++ main/Const.m
@@ -1,8 +1,8 @@
 classdef Const
     properties (Constant)
-      e0 = 8.8541878176e-12;            %[F/m] permissividade no espaço livre 
-      u0 = 4*pi*1e-7;                   %[H/m] permeabilidade no espaço livre
-      c0 = 1/sqrt(Const.e0 * Const.u0);   %[m/s] velocidade da luz no espaço livre
-      eta0 = sqrt(Const.u0/Const.e0);   %[Ohm] impedancia caracterisitca do espaço livre      
+      e0 = 8.8541878176e-12;            %[F/m] permissivity in free space 
+      u0 = 4*pi*1e-7;                   %[H/m] permeability in free space
+      c0 = 1/sqrt(Const.e0 * Const.u0); %[m/s] light speed in free space
+      eta0 = sqrt(Const.u0/Const.e0);   %[Ohm] caracteristic impedance in free space  
     end
 end
