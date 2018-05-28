59347ee9bfb801378273d308af3a72f44e6b2904
Marcelo Bertoldi Diani <marcelodianib@gmail.com>
2018-05-27 21:14:32 -0300

    Removing old files

--- Fibra_Optica/function_calc_pcore.m
+++
@@ -1,6 +0,0 @@
-function pcore = function_calc_pcore (nu,neff,k0,n1,n2,a)
-u = k0*sqrt(n1^2-neff.^2);
-na = sqrt(n1^2-n2^2);
-v = k0*a*na;
-pcore = (1-((u)/(v)).^2).*(1-(besselj(nu,u*a).^2)./(besselj(nu+1,u*a).*besselj(nu-1,u*a)));
-end
